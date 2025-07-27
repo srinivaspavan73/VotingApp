2742e0b6bc78b188abb9f6b6c42c58e11f3370ea
Bitra Pavan Kumar <100582064+srinivaspavan73@users.noreply.github.com>
2025-07-28 00:51:11 +0530

    CSS file name updated in the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './VoteApp/styles.css'
+import './VoteApp/Styles.css'
 import VotingApp from './VoteApp/VotingApp'
 
 createRoot(document.getElementById('root')).render(
